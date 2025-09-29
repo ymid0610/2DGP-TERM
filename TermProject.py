4a3e4b5f404674950640a12c13bf605a84335ecf
ymid0610 <ymid0610@gmail.com>
2025-09-29 21:33:53 +0900

    Initial commit

---
+++ TermProject.py
@@ -0,0 +1,14 @@
+from pico2d import *
+import random
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+open_canvas()
+close_canvas()
