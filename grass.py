3426296e0aadb4f80cf2d1a1027b8da23433e0aa
ymid0610 <ymid0610@gmail.com>
2025-11-26 03:43:12 +0900

    grass.py bb 수정

--- grass.py
+++ grass.py
@@ -3,7 +3,7 @@ from pico2d import *
 class Grass:
     def __init__(self):
         self.x = 240
-        self.y = 48
+        self.y = 300
         self.image = load_image('Resource/Map/Grass.png')
 
     def update(self):
@@ -14,7 +14,7 @@ class Grass:
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
-        return self.x - 240, self.y - 24, self.x + 240, self.y + 24
+        return self.x - 240, self.y + 24, self.x + 240, self.y + 24
 
     def handle_collision(self, group, other):
         pass
