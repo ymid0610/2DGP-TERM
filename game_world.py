20707a3f2824b3c61d8044c3ed7bc7c280ee77a8
ymid0610 <ymid0610@gmail.com>
2025-11-26 01:37:23 +0900

    충돌 처리를 위한 game_world 업데이트

--- game_world.py
+++ game_world.py
@@ -1,21 +1,10 @@
-# layer 0: Background Objects
-# layer 1: Foreground Objects
-
-world = [[],[]]
+world = [[] for _ in range(4)]
 
 def add_object(o, depth = 0):
     world[depth].append(o)
 
 def add_objects(ol, depth = 0):
     world[depth] += ol
-
-def remove_object(o):
-    for layer in world:
-        if o in layer:
-            layer.remove(o)
-            return
-
-    raise ValueError("'Cannot delete non existing object")
 
 def update():
     for layer in world:
@@ -26,3 +15,58 @@ def render():
     for layer in world:
         for o in layer:
             o.draw()
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+    raise ValueError('Cannot delete non existing object')
+
+def clear():
+    global world
+    for layer in world:
+        layer.clear()
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    return True
+
+collision_pairs = {}
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:
+        collision_pairs[group] = [[], []]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+    return None
+
+def handle_collision():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
+
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            remove_collision_object(o)
+            return
+    raise ValueError('Cannot delete non existing object')
