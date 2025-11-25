f484ca41a19af1116eaeaa96bfdba7dde8b129dc
ymid0610 <ymid0610@gmail.com>
2025-11-26 01:37:03 +0900

    기본 Grass 구현 완료

---
+++ grass.py
@@ -0,0 +1,20 @@
+from pico2d import *
+
+class Grass:
+    def __init__(self):
+        self.x = 240
+        self.y = 48
+        self.image = load_image('Resource/Map/Grass.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 240, self.y - 24, self.x + 240, self.y + 24
+
+    def handle_collision(self, group, other):
+        pass
